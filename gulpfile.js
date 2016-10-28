8b702d814344f59ee5451658177962b9e51a911b
MaciejKawczynski <sqars89@gmail.com>
2016-10-28 20:42:12 +0200

    Inital commit, gulp configuration, folder structure

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var vinylSourceStream = require('vinyl-source-stream');
+var vinylBuffer = require('vinyl-buffer');
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('lint', function(){
+  return gulp.src('src/js/app.js')
+  .pipe(jshint({esversion: 6}))
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify', function() {
+    var sources = browserify({
+            entries: 'src/js/app.js',
+            debug: true
+        })
+        .transform(babelify.configure({
+            presets: ['es2015'],
+            comments: true,
+						compact: false
+        }));
+
+    return sources.bundle()
+        .pipe(vinylSourceStream('app.js'))
+        .pipe(vinylBuffer())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('default', function() {
+    gulp.watch('src/js/**/*.js', ['browserify']);
+    gulp.watch('src/js/**/*.js', ['lint']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
