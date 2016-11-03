7ceeb4fb7868f018ae1e4e3d382d9a3a2791069a
MaciejKawczynski <sqars89@gmail.com>
2016-11-03 22:01:27 +0100

    add functionality to smoothly move frogger

--- src/js/Components/Board/Board.js
+++ src/js/Components/Board/Board.js
@@ -10,83 +10,24 @@ export default class Board {
         this.board = document.getElementById('canvas');
         this.context = this.board.getContext("2d");
         this.frogger = new Frogger(this.board);
+        this.froggerMoving = false;
         this.cars = CarService.createCars();
-        this.turtles = TurtleService.createTurtles();
-        this.water = WaterService.createWater();
-        this.wood = WoodService.createWood();
-        this.sailElement = null;
     };
 
     setBoard() {
         this.context.clearRect(0, 0, this.board.width, this.board.height);
         this.frogger.drawFrogger(this.context);
+        this.froggerMoving ? this.moveFrogger() : false;
         requestAnimationFrame(this.setBoard.bind(this));
-        // this.board = document.querySelectorAll('#board div');
-        // BoardService.clearBoard(this.board);
-        // this.water.forEach(waterObj => waterObj.setWaterPosition(this.board));
-        // this.sailElement ? this.sailElement.sailFrogger(this.frogger) : false;
-        // this.turtles.forEach(turtle => turtle.setTurtlePosition(this.board));
-        // this.wood.forEach(wood => wood.setWoodPosition(this.board));
-        // this.frogger.setFroggerPosition(this.board);
-        // this.cars.forEach(car => car.setCarPosition(this.board));
-        // this.checkCollision();
     };
 
-    moveFrogger(event) {
-        this.frogger.move(event);
-        let turtleCollision = BoardService.checkCollision(this.frogger, this.turtles);
-        let woodCollision = BoardService.checkCollision(this.frogger, this.wood);
-        if (turtleCollision) {
-            let sailTurtle = this.turtles.filter(turtle => turtle.getPosition() === turtleCollision);
-            this.sailElement = sailTurtle[0];
-        } else if (woodCollision) {
-            let sailWood = this.wood.filter(wood => wood.getPosition() === woodCollision);
-            this.sailElement = sailWood[0];
-        } else {
-            this.sailElement = null;
-        };
-        this.setBoard();
-    };
+    setFroggerMove(event){
+      let isMoving = this.frogger.setDirection(event);
+      isMoving ? this.froggerMoving = true : false;
+    }
 
-    checkCollision() {
-        let collision = false;
-        let carCollision = BoardService.checkCollision(this.frogger, this.cars);
-        let waterCollision = BoardService.checkCollision(this.frogger, this.water);
-        let turtleCollision = BoardService.checkCollision(this.frogger, this.turtles);
-        let woodCollision = BoardService.checkCollision(this.frogger, this.wood);
-        carCollision !== false || waterCollision !== false ? collision = true : false; // TODO: check this condition
-        turtleCollision ? collision = false : false;
-        woodCollision ? collision = false : false;
-        return collision;
-    };
-
-    startMovingLine(objects, line, speed = 1000) {
-        return window.setInterval(() => {
-            let filteredLine = objects.filter(obj => obj.line == line);
-            filteredLine.forEach(obj => obj.move());
-            this.setBoard();
-        }, speed); // TODO: add speed functionality
-    };
-
-    startBoard() {
-        for (let i = 1, speed = 1100; i <= 5; i++) {
-            this.startMovingLine(this.cars, i, speed);
-            speed = speed - 100;
-        };
-        for (let i = 1, speed = 900; i <= 2; i++) {
-            this.startMovingLine(this.turtles, i, speed);
-            speed = 700;
-        };
-        for (let i = 1, speed = 900; i <= 3; i++) {
-            this.startMovingLine(this.wood, i, speed);
-            speed = speed - 200;
-        };
-        let divingTurtles = this.turtles.filter(turtle => turtle.diving);
-        window.setInterval(() => {
-            divingTurtles.forEach((turtle) => {
-                turtle.dived = !turtle.dived;
-            });
-        }, 1000);
+    moveFrogger() {
+      this.froggerMoving = this.frogger.move(this.frogger.direction);
     };
 
 }
--- src/js/Components/Frogger.js
+++ src/js/Components/Frogger.js
@@ -1,49 +1,71 @@
 import MovingObject from './MovingObject.js';
 
-export default class Frogger extends MovingObject{
-  constructor(board, posX, posY, direction, lives){
-    super();
-    this.height = 50;
-    this.width = 50;
-    this.posX = board.width * 0.5;
-    this.posY = board.height - this.height;
-    this.direction = 'up';
-    this.lives = 3;
-  };
-
-  drawFrogger(ctx) {
-    ctx.beginPath();
-    ctx.rect(this.posX, this.posY, 50, 50);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-}
-
-  setFroggerPosition(board){
-    board[this.getPosition()].className = "frogger";
-  };
-
-  move(event){
-    switch(event.which){
-      case 37:
-        this.direction = 'left';
-        this.posX--;
-        break;
-      case 38:
+export default class Frogger extends MovingObject {
+    constructor(board, posX, posY, direction, lives) {
+        super();
+        this.height = 50;
+        this.width = 50;
+        this.posX = board.width * 0.5;
+        this.posY = board.height - this.height;
         this.direction = 'up';
-        this.posY--;
-        break;
-      case 39:
-        this.direction = 'right';
-        this.posX++;
-        break;
-      case 40:
-        this.direction = 'down';
-        this.posY++;
-        break;
-      default:
-        break;
+        this.lives = 3;
+        this.movingCount = 0;
     };
-  }
+
+    drawFrogger(ctx) {
+        ctx.beginPath();
+        ctx.rect(this.posX, this.posY, 50, 50);
+        ctx.fillStyle = "green";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    setDirection(event) {
+        let result = false;
+        switch (event.which) {
+            case 37:
+                this.direction = 'left';
+                result = true;
+                break;
+            case 38:
+                this.direction = 'up';
+                result = true;
+                break;
+            case 39:
+                this.direction = 'right';
+                result = true;
+                break;
+            case 40:
+                this.direction = 'down';
+                result = true;
+                break;
+            default:
+                result = false;
+        };
+        return result;
+    }
+
+    move(direction) {
+        let result = false;
+        switch (direction) {
+            case 'left':
+                this.posX -= 2;
+                break;
+            case 'up':
+                this.posY -= 2;
+                break;
+            case 'right':
+                this.posX += 2;
+                break;
+            case 'down':
+                this.posY += 2;
+                break;
+            default:
+                break;
+        };
+        this.movingCount++;
+        this.movingCount < 25 ? result = true : this.movingCount = 0;
+        return result;
+    }
 
 }
